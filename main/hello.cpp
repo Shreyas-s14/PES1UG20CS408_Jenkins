99ab33d74c41d4d9adc0e0439a8a708d72cdb143
Shreyas S <79797969+Shreyas-s14@users.noreply.github.com>
2023-02-19 17:55:40 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -6,6 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run\n";
-  cout << "SRN:PES1UG20CS408\n"
+  cout << "SRN:PES1UG20CS408\n";
   return 0;
 }
